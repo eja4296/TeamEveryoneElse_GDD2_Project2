7f68a0b2aa2fcc4d7f5952b51230eecf4e526e5b
Erik Azzarano (RIT Student) <eja4296@ad.rit.edu>
2018-02-26 13:33:15 -0500

    Trying to fix everything

---
+++ GDD2_Project2/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Camera Movement Class
+// Camera follows movement of robber
+public class CameraMovement : MonoBehaviour {
+	// Attributes
+	public GameObject robber;
+	public float cameraHeight;
+	public float cameraOffset;
+
+	// Use this for initialization
+	void Start () {
+		cameraHeight = 20f;
+		cameraOffset = -20f;
+		this.transform.position = new Vector3 (0f, cameraHeight, cameraOffset);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Update the camera's x and z position to match the robber's movement
+		float robberX = robber.transform.position.x;
+		float robberZ = robber.transform.position.z;
+		float newCameraZ = robberZ + cameraOffset;
+		this.transform.position = new Vector3 (robberX, cameraHeight, newCameraZ);
+	}
+}
---
+++ GDD2_Project2/Assets/Scripts/Robber.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Robber class
+// Inherit Character base class
+public class Robber : Character {
+	// Attributes
+	public Camera camera;
+	public float height;
+	public bool moving;
+
+	// Use this for initialization
+	override public void Start () {
+		movementSpeed = 0.5f;
+		rotationSpeed = 3f;
+		direction = new Vector3 (0, 0, 1);
+		height = 0.5f;
+		moving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetInput ();
+
+	}
+
+	// Get User Input
+	void GetInput(){
+		// Get the mouse position and find where it hits in the game
+		Vector3 mousePos = Input.mousePosition;
+		Ray mouseRay = camera.ScreenPointToRay (mousePos);
+		RaycastHit hit;
+
+		if (Physics.Raycast (mouseRay, out hit, 100f)) {
+			Vector3 targetDirection = hit.point;
+			Vector3 newDirection = targetDirection - this.transform.position;
+
+			// Don't move or rotate the robber if it is already at the mouse's position (or close to it)
+			if (newDirection.magnitude > 2) {
+				targetDirection.y = height;
+				Rotate (targetDirection);
+
+				// Handle Movement
+				// Forward (W)
+				if(Input.GetKey(KeyCode.W)){
+					Vector3 moveVec = this.transform.forward * movementSpeed;
+					Move (moveVec);
+					moving = true;
+				}
+
+				// It doesn't really make sense to have any other movement because the mouse can handle that, but it's here if we change our mind
+				/*
+				// Backward (S)
+				if(Input.GetKey(KeyCode.S)){
+					Vector3 moveVec = this.transform.forward * -movementSpeed;
+					Move (moveVec);
+					moving = true;
+				}
+				// Left (A)
+				if(Input.GetKey(KeyCode.A)){
+					Vector3 moveVec = this.transform.right * -movementSpeed;
+					Move (moveVec);
+					moving = true;
+				}
+				// Right (D)
+				if(Input.GetKey(KeyCode.D)){
+					Vector3 moveVec = this.transform.right * movementSpeed;
+					Move (moveVec);
+					moving = true;
+				}
+				*/
+
+			}
+		}
+	}
+
+	// Rotate the robber based on mouse
+	override public void Rotate(Vector3 rotation){
+		this.transform.LookAt(rotation);	
+	}
+}
