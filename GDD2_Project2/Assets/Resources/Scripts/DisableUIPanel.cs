cd56791915345d0d3eca2d354d5120c4533aec0e
Erik Azzarano <eja4296@rit.edu>
2018-03-24 16:39:40 -0400

    start menu with controls

---
+++ GDD2_Project2/Assets/Resources/Scripts/DisableUIPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableUIPanel : MonoBehaviour {
+
+	public GameObject controlsPanel;
+
+	// Use this for initialization
+	void Start () {
+		controlsPanel = GameObject.Find ("ControlsPanel");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick(){
+		//controlsPanel.gameObject.SetActive (false);
+		controlsPanel.GetComponent<CanvasGroup> ().alpha = 0;
+		controlsPanel.GetComponent<CanvasGroup> ().interactable = false;
+		controlsPanel.GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+}
---
+++ GDD2_Project2/Assets/Resources/Scripts/EnableUIPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableUIPanel : MonoBehaviour {
+	public GameObject controlsPanel;
+
+	// Use this for initialization
+	void Start () {
+		controlsPanel = GameObject.Find ("ControlsPanel");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnClick(){
+		//controlsPanel.gameObject.SetActive (true);
+		controlsPanel.GetComponent<CanvasGroup> ().alpha = 1;
+		controlsPanel.GetComponent<CanvasGroup> ().interactable = true;
+		controlsPanel.GetComponent<CanvasGroup> ().blocksRaycasts = true;
+	}
+}
