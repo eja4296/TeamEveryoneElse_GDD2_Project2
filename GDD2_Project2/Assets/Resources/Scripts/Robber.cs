2b6e67f454aaa0a586e2377fc0bc479a697eca1b
Erik Azzarano <eja4296@rit.edu>
2018-03-27 18:44:40 -0400

    Fixed restart issue

--- GDD2_Project2/Assets/Resources/Scripts/Robber.cs
+++ GDD2_Project2/Assets/Resources/Scripts/Robber.cs
@@ -46,7 +46,11 @@ public class Robber : Character {
 		walkAudio.loop = true;
 		moving = false;
 		pauseMenu = GameObject.Find ("PauseMenu");
+		pauseMenu.GetComponent<CanvasGroup> ().alpha = 0;
+		pauseMenu.GetComponent<CanvasGroup> ().interactable = false;
+		pauseMenu.GetComponent<CanvasGroup> ().blocksRaycasts = false;
 		paused = false;
+		Time.timeScale = 1f;
 	}
 
 	// Update is called once per frame
