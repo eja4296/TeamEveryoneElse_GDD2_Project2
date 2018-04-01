4ca246b7be9989941a6b7525446523926b19848c
Joe Wang <jxw6096@rit.edu>
2018-04-01 07:15:00 -0400

    Add files via upload

---
+++ GDD2_Project2/Assets/Resources/Prefabs/Inventory/useBoost.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class useBoost : MonoBehaviour
+{
+    public GameObject robbie;
+    Robber rob;
+    float timer = 10;
+    bool boost = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        robbie = GameObject.FindGameObjectWithTag("Player");
+        rob = robbie.GetComponent<Robber>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            if (System.Int32.Parse(this.transform.Find("Text").GetComponent<Text>().text) > 0)
+            {
+                Boost();
+                boost = true;
+            }
+        }
+
+        if (boost)
+        {
+            if (timer >= 0)
+            {
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                    rob.movementSpeed = 400f;
+                else
+                    rob.movementSpeed = 200f;
+
+                timer -= Time.deltaTime;
+            }
+
+            else if (timer <= 0)
+                Destroy(this.gameObject);
+
+            else
+            {
+                timer = 5;
+                boost = false;
+            }
+        }
+	}
+
+    void Boost()
+    {
+        string temp = this.transform.Find("Text").GetComponent<Text>().text;
+        if (System.Int32.Parse(temp) > 1)
+        {
+            int tcount = System.Int32.Parse(temp) - 1;
+            this.transform.Find("Text").GetComponent<Text>().text = "" + tcount;
+        }
+        //else
+          //  Destroy(this.gameObject);
+    }
+}
---
+++ GDD2_Project2/Assets/Resources/Prefabs/Inventory/useDisguise.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class useDisguise : MonoBehaviour
+{
+    public GameObject robber;
+    float time = 10;
+    SkinnedMeshRenderer m;
+    public bool disguise = false;
+    float count = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        robber = GameObject.FindGameObjectWithTag("mesh");
+        m = robber.GetComponent<SkinnedMeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            if (System.Int32.Parse(this.transform.Find("Text").GetComponent<Text>().text) > 0)
+            {
+                Disguise();
+                disguise = true;
+            }
+        }
+
+        if (disguise)
+        {
+            if (time >= 0)
+            {
+                toggleMaterial(m);
+
+                time -= Time.deltaTime;
+            }
+            else if (time <= 0)
+            {
+                toggleMaterial(m);
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                toggleMaterial(m);
+                time = 10;
+                disguise = false;
+            }
+        }
+	}
+
+    void toggleMaterial(SkinnedMeshRenderer m)
+    {
+        Material cop = Resources.Load("Models/Materials/Cop", typeof(Material)) as Material;
+        Material robber = Resources.Load("Materials/Robber", typeof(Material)) as Material;
+
+        if (time >= 0)
+            m.material = cop;
+        else
+            m.material = robber;
+    }
+
+    void Disguise()
+    {
+        if (System.Int32.Parse(this.transform.Find("Text").GetComponent<Text>().text) > 1)
+        {
+            int tcount = System.Int32.Parse(this.transform.Find("Text").GetComponent<Text>().text) - 1;
+            this.transform.Find("Text").GetComponent<Text>().text = "" + tcount;
+        }
+    }
+}
